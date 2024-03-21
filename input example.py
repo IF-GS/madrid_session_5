baf65227574d58c83c41610ece90b9da81caad80
Isaac Gonzalez <isaacgzz@MacBook-Pro-de-Isaac-3.local>
2024-03-21 18:06:24 +0100

    Initial commit

---
+++ input example.py
@@ -0,0 +1,20 @@
+name = input("what is your name? ")
+age = input("what is your age? ")
+# print("Hello", ", name, "!", sep="")
+
+try:
+age = int(age) #convert string to integer
+birth_year = 2024 - age
+print(name, ", you were born in ", birth_year, ".", sep="")
+number = input("give me a number to divide the age ")
+number = int(number)
+print(age/number)
+
+except ValueError:
+    print("Invalid age. Please enter a number. ")
+except ZeroDivisionError
+    print("you cannot divide by zero")
+    else
+    print("no exceptions were raised")
+    finally
+    print("thankyou for playing")
